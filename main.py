adf35b11b028a0385774fd088d48d32a11befaf0
a <antoniomusciano@gmail.com>
2024-02-12 19:46:34 -0500

    shoutout bing i hate google

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from bing_image_downloader import downloader
+import os
+import tkinter as tk
+from PIL import Image, ImageTk
 
 
+def fetch_images(query):
+    downloader.download(query, limit=5, output_dir='dataset', adult_filter_off=True, force_replace=False, timeout=60,
+                        verbose=True)
 
-def fetch_images():
-    pass
 
-
-def display_images():
+def display_images(folder):
     pass
 
 
 if __name__ == "__main__":
-    cORp = input("Enter c or p: ") # cow or pig
+    choice = input("Enter c or p: ")  # cow or pig
+    if choice == 'c':
+        choice = 'cow'
+        adj = input("Please enter an adjective (preferably a nice one): ")
 
+    if choice == 'p':
+        choice = 'pig'
+        adj = input("Please enter an adjective (preferably a nice one): ")
 
+    fetch_images(adj + choice)
+    display_images("dataset/" + adj + choice)
